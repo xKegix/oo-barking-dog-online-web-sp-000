09d301325b84afb51d1476bad38032c46d649059
Keginold Philip <keginoldp@hotmail.com>
2021-02-04 14:57:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 # Your code goes here!
 class Dog
 	def dname=(give_dog_name)
-		dog_name = give_dog_name
+		@dog_name = give_dog_name
 	end
 
 	def name
-		puts "dog_name"
+		@puts dog_name
 	end
 
 	def bark
